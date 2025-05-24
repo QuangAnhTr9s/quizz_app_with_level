2a95850fd9b27f2b057555ef6299e9849a69a92c
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2025-05-24 15:40:02 +0700

    handle level screen

--- lib/feature/level/level_list_screen.dart
+++ lib/feature/level/level_list_screen.dart
@@ -124,83 +124,80 @@ class _LevelListScreenState extends State<LevelListScreen> {
                           padding: EdgeInsets.only(bottom: 20.h),
                           separatorBuilder: (_, __) => SizedBox(height: 10.h),
                           itemBuilder: (context, index) {
-                            if (state is UserLoaded) {
-                              final cat = item[index];
-                              bool isDone = cat.isDone;
-                              return InkWell(
-                                onTap: () {
-                                  Navigator.of(context)
-                                      .push(MaterialPageRoute(
-                                        builder: (context) =>
-                                            const QuizScreen(),
-                                      ))
-                                      .then(
-                                        (value) => _levelCubit.loadLevels(),
-                                      );
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(12.w),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(8.r),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      /// avatar
-                                      ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(8.r),
-                                        child: Container(
-                                          width: 80.w,
-                                          height: 80.w,
-                                          decoration: BoxDecoration(
-                                            color: Colors.grey.shade300,
-                                          ),
-                                          child: cat.avatar != null &&
-                                                  cat.avatar!.isNotEmpty
-                                              ? Image.asset(
-                                                  "assets/${cat.avatar!}",
-                                                  fit: BoxFit.fill,
-                                                )
-                                              : const SizedBox(),
-                                        ),
-                                      ),
-                                      SizedBox(width: 12.w),
-
-                                      /// info
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(cat.name ?? '',
-                                                style: TextStyle(
-                                                    fontSize: 12.sp,
-                                                    fontWeight: FontWeight.w400,
-                                                    fontFamily:
-                                                        GoogleFonts.jotiOne()
-                                                            .fontFamily)),
-                                            SizedBox(height: 8.h),
-                                            InfoLevel(cat: cat),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(width: 6.w),
-
-                                      /// checked icon
-                                      if (isDone)
-                                        Image.asset(
-                                          'assets/ic_checked.png',
-                                          height: 24.w,
-                                          width: 24.w,
-                                        ),
-                                    ],
-                                  ),
+                            final cat = item[index];
+                            bool isDone = cat.isDone;
+                            return InkWell(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(
+                                      builder: (context) => const QuizScreen(),
+                                    ))
+                                    .then(
+                                      (value) => _levelCubit.loadLevels(),
+                                    );
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(12.w),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8.r),
                                 ),
-                              );
-                            } else {
-                              return const SizedBox.shrink();
-                            }
+                                child: Row(
+                                  children: [
+                                    /// avatar
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(8.r),
+                                      child: Container(
+                                        width: 80.w,
+                                        height: 80.w,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey.shade300,
+                                        ),
+                                        child: cat.avatar != null &&
+                                                cat.avatar!.isNotEmpty
+                                            ? Image.asset(
+                                                "assets/${cat.avatar!}",
+                                                fit: BoxFit.fill,
+                                              )
+                                            : const SizedBox(),
+                                      ),
+                                    ),
+                                    SizedBox(width: 12.w),
+
+                                    /// info
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(cat.name ?? '',
+                                              style: TextStyle(
+                                                  fontSize: 12.sp,
+                                                  fontWeight: FontWeight.w400,
+                                                  fontFamily:
+                                                      GoogleFonts.jotiOne()
+                                                          .fontFamily)),
+                                          SizedBox(height: 8.h),
+                                          InfoLevel(cat: cat),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(width: 6.w),
+
+                                    /// checked icon
+                                    if (isDone)
+                                      ConstrainedBox(
+                                        constraints: BoxConstraints(
+                                            maxHeight: 24.w, maxWidth: 24.w),
+                                        child: Image.asset(
+                                          'assets/ic_checked.png',
+                                          fit: BoxFit.fill,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                              ),
+                            );
                           },
                         );
                       } else {
